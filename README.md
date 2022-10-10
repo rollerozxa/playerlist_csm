7095bc9a1f66d45f5d05dba38a97bbd50c28cdcd
ROllerozxa <rollerozxa@voxelmanip.se>
2022-10-10 21:36:31 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# playerlist_csm
+Fleckenstein's playerlist mod ported to a clientmod.
+
+Requires a server that has the READ_PLAYERINFO CSM restriction disabled.
---
+++ init.lua
@@ -0,0 +1,43 @@
+
+local huds_store = {}
+
+local list_shown = false
+
+minetest.register_globalstep(function(dtime)
+	local sneak_held = minetest.localplayer:get_control().sneak
+
+	if sneak_held and not list_shown then
+		local huds = {minetest.localplayer:hud_add({
+			hud_elem_type = "image",
+			position = {x = 0.5, y = 0},
+			offset = {x = 0, y = 20},
+			text = "^[resize:8x8^[colorize:#111:255",
+			alignment = {x = 0, y = 1},
+			scale = {x = 40, y = #minetest.get_player_names() * 4},
+			number = 0xFFFFFF,
+		})}
+
+		for i, name in ipairs(minetest.get_player_names()) do
+			if i ~= 1 then -- for some reason the first name in the list is a duplicate of the current player
+				table.insert(huds, minetest.localplayer:hud_add({
+					hud_elem_type = "text",
+					position = {x = 0.5, y = 0},
+					offset = {x = 0, y = 23 + (i - 2) * 18},
+					text = name,
+					alignment = {x = 0, y = 1},
+					scale = {x = 100, y = 100},
+					number = color or 0xFFFFFF,
+				}))
+			end
+		end
+		huds_store = huds
+
+		list_shown = true
+	elseif not sneak_held and list_shown then
+		for _, id in pairs(huds_store) do
+			minetest.localplayer:hud_remove(id)
+		end
+
+		list_shown = false
+	end
+end)
